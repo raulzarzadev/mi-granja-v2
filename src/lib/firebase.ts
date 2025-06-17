b55f8add4e496bc3281e584712d94fb40b7712de
Raul Zarza <raulzarza.dev@gmail.com>
2025-06-17 09:10:10 -0700

    refactor: Simplificar la configuración de Firebase utilizando variables de entorno

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,9 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig = {
-  // TODO: Agregar las credenciales de Firebase
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-}
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
+  ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
+  : '{}'
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig)
