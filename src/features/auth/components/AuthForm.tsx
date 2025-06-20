ae5212212e90b187186f50d71092946462f49712
Raul Zarza <raulzarza.dev@gmail.com>
2025-06-19 17:31:30 -0700

    refactor: eliminar consola de depuración en el componente AuthForm

--- src/features/auth/components/AuthForm.tsx
+++ src/features/auth/components/AuthForm.tsx
@@ -111,8 +111,6 @@ const AuthForm: React.FC = () => {
     }
   }
 
-  console.log({ formErrors, authError: error, emailLinkSent })
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
